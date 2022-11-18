982809175fde8dc315dca21181d18a3bd7474cea
Bruno Rodrigues Da Silva <Brunochh@outlook.com>
2022-11-18 18:46:33 -0300

    Add files via upload

---
+++ Calcular_Area.py
@@ -0,0 +1,4 @@
+largura = float(input('Largura da área: '))
+altura = float(input('altura da área: '))
+area = largura * altura
+print(f'Sua área tem a dimensão de {largura}x{altura} e sua área e de {area}m²')
